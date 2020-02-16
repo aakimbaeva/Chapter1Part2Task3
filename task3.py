550cbab7ca950e46487386719fb81ff5c0a59bf1
Azhara <mac@MacBook-Air-mac-AJara.local>
2020-02-16 23:18:54 +0600

    first commit

---
+++ task3.py
@@ -0,0 +1,10 @@
+number = int(input('Write a number: '))
+print('The next number for the number', str(number), 'is', number + 1, '\nThe previous number for the number', str(number), 'is', number - 1)
+
+
+#def info(number):
+#    result1 = number + 1
+#    result2 = number - 1
+#    print(f'The next number for the number {number} is {result1} and the previous number for the number {number} is {result2}')
+
+#info(5)
